6a79fd81d087ef6fbf8a5987ff552124bd5e2c80
Luciano Garbino <lucigar01@gmail.com>
2020-05-30 16:20:32 -0300

    Co-authored-by: FedeXX GalanT <fedegalant@gmail.com>

--- 2013/tp4/15/left_trim.c
+++ 2013/tp4/15/left_trim.c
@@ -5,19 +5,21 @@ void left_trim (char *s) {
 
     int size_t;
     size_t = sizeof(s)/sizeof(s[0]);
-
-    printf("String en funcion: %s\n", s);
+    char str_final [size_t];
+   
 
     for (int i = 0; i <size_t; i++ ){
-
-        if (s[i] == " "){
-
-            
-
-
+        if (s[i] != '\0'){
+            if (s[i] != ' '){
+                printf("IN\n");
+                str_final[i] = s[i];
+            }
+            else{   
+                s[i] = s[i+1];
+                i--;
+            }
         }
-        
-        
     }
-
+    
+    printf("String en funcion:%s\n", str_final);
 }
