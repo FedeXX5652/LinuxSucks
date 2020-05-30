6cdc14cd82d4cc5f58ffa929df55ea386048321c
Luciano Garbino <lucigar01@gmail.com>
2020-05-30 17:08:09 -0300

    cambios

--- 2013/tp4/15/left_trim.c
+++ 2013/tp4/15/left_trim.c
@@ -4,21 +4,18 @@
 void left_trim (char *s) {
 
     int size_t;
-    size_t = sizeof(s)/sizeof(s[0]);
+    size_t = (int)(sizeof(s));
     char str_final [size_t];
+
+    printf("Size: %d\n",size_t);
    
 
     for (int i = 0; i <size_t; i++ ){
-        if (s[i] != '\0'){
+        
             if (s[i] != ' '){
                 printf("IN\n");
                 str_final[i] = s[i];
             }
-            else{   
-                s[i] = s[i+1];
-                i--;
-            }
-        }
     }
     
     printf("String en funcion:%s\n", str_final);
