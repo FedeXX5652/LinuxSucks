4738e90996c06d7efab87483809197b50b43f40b
Luciano Garbino <lucigar01@gmail.com>
2020-06-13 11:37:45 -0300

    Co-authored-by: FedeXX GalanT <fedegalant@gmail.com>

---
+++ APUNTES/memoria_dinamica.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//Memoria dinamica (lo saque de un video de youtube porque nacho explica para el orto)
+
+int main (){
+
+
+int N = 10;
+int *vector;
+
+vector = (int*)malloc(N*sizeof(int));
+printf("");
+
+
+
+return(0);
+}
+
+
+
+
+
+
+
