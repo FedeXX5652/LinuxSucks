5f5c0e85b6f05addc66f1fd1dee6dd7ea37bdffb
Luciano Garbino <lucigar01@gmail.com>
2020-07-12 15:33:26 -0300

    lucho

--- FedeXX/ej-Carlos-1/func.c
+++ FedeXX/ej-Carlos-1/func.c
@@ -1,1 +1,38 @@
 #include "link.h"
+
+int operator(int *num){
+    
+    int suma=0;
+    float prom=0;
+    int mayor=num[0];
+    int menor=num[0];
+    int TAM=(sizeof(num)/sizeof(num[0]));
+    printf("TAMAÑO: %d\n",TAM);
+
+    for(int i=0; i<TAM; i++){
+        suma = suma + num[i];
+    }
+
+    for(int i=0; i<TAM; i++){
+        prom = (float)suma/TAM;
+    }
+
+    for(int i=0; i<TAM; i++){
+        if(mayor < num[i]){
+            mayor = num[i];
+        }
+
+        if(menor > num[i]){
+            menor = num[i];
+        }
+    }
+
+    
+    
+    printf("SUMA: %d\n", suma);
+    printf("PROMEDIO: %.2f\n",prom);
+    printf("MAYOR: %d\n", mayor);
+    printf("MENOR: %d\n", menor);
+    
+    return 0;
+}
--- FedeXX/ej-Carlos-1/prog.c
+++ FedeXX/ej-Carlos-1/prog.c
@@ -1,3 +1,47 @@
+/*1. Realizar un programa que por línea de comando reciba una cantidad indeterminada de argumentos. Los mismos serán considerados como términos de una serie del tipo entero.
+Se deberá presentar por stdout las siguientes operaciones:
+
+-Suma de todos los términos
+-Promedio de todos los términos
+-El mayor de todos
+-El menor de todos
+
+Se tiene que validar los errores que considere pertinente terminado el programa con un código
+de error para cada caso. Si la finalización es exitosa, terminará con código de retorno 0. El
+programa deberá tener un solo punto de salida.*/
+
 #include "link.h"
 
-int main ()
+
+
+int main (int argc, char *argv[]){
+
+    
+    int error = 0;
+    int num[argc-1];
+
+    for ( int i=0 ; i<argc-1 ; i++ ){
+        num[i]=0;
+    }
+
+
+    for ( int i=1 ; i<argc ; i++ )
+    {
+        
+
+        num[i-1] = atol(argv[i]);
+
+    }
+
+    for ( int i=0 ; i<argc-1 ; i++ )
+    {
+        printf("Numero %d = %d\n\n",i,num[i]);
+    }
+
+    error = operator(num);
+
+    return error;
+
+    
+    
+}
