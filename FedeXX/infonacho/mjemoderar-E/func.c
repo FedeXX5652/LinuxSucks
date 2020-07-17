abfbc4e962e58160af31bf87dad8f799e3239e72
Luciano Garbino <lucigar01@gmail.com>
2020-07-17 16:27:52 -0300

    Co-authored-by: fedegalant5652@hotmail.com <fedegalant5652@hotmail.com>

--- FedeXX/infonacho/mjemoderar-E/func.c
+++ FedeXX/infonacho/mjemoderar-E/func.c
@@ -33,15 +33,32 @@ void func(char **proh, char **argv, int cant, int argc){
     
 
     for(int w=0;w<=cant;w++){
+
+        p = *(proh+w);
+
+        for (int o=0 ; *( p+o ) != '\0' ; o++ )
+        {
+            if(*(p+o) == '\n'){
+                *(p+o) = 0;
+            }
+        }
+
+        
         for (int y = 1; y < argc; y++)
         {
             strcat(argv[y], "\0");
             printf("arg: %ld\n", strlen(argv[y]));
             printf("proh: %ld\n",strlen(proh[w]));
             
-            if (strcmp(argv[y], proh[w])==0)
+            
+            if (0 == (strcmp(argv[y], proh[w])))
             {
-                printf("entre\n");
+                p = *(argv+y);
+
+                for (int o=0 ; *( p+o ) != '\0' ; o++ )
+                {
+                    *(p+o) = 'X';
+                }
                 coincidencias++;
             }
         } 
@@ -53,15 +70,14 @@ void func(char **proh, char **argv, int cant, int argc){
     printf("--------------------------------------------------------\n\n");
     
     i=0;
+    printf("PALABRA CAMBIADAS: \n");
+    
     do{
-
-        printf("PALABRA CAMBIADAS: %s\n", *(argv+i+1));
-        printf("\n");
+        printf("%s ", *(argv+i+1));
         i++;
-
     }
     while(*(argv+i+1) != NULL);
-
+    printf("\n");
     printf("Caracteres cambiados: %d\n", caracX);
 
 }
